e93e2457f0668239cc29d61ca4f4492190da5705
GadzhievSA <s.gadzhiev@id-mt.ru>
2020-02-02 15:00:46 +0300

    Web hook url logging

--- src/main/java/ru/gadjini/reminder/properties/WebHookProperties.java
+++ src/main/java/ru/gadjini/reminder/properties/WebHookProperties.java
@@ -1,9 +1,13 @@
 package ru.gadjini.reminder.properties;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties("webhook")
 public class WebHookProperties {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(WebHookProperties.class);
 
     private String externalUrl;
 
@@ -15,6 +19,7 @@ public class WebHookProperties {
 
     public void setExternalUrl(String externalUrl) {
         this.externalUrl = externalUrl;
+        LOGGER.info("WebHook external url: " + externalUrl);
     }
 
     public String getInternalUrl() {
