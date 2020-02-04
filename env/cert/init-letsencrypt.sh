f2d92f8a8664763da41d61d19e6f3b88b2141dea
GadzhievSA <s.gadzhiev@id-mt.ru>
2020-02-04 22:00:43 +0300

    Фикс сборки для продуктива

--- env/cert/init-letsencrypt.sh
+++ env/cert/init-letsencrypt.sh
@@ -3,7 +3,7 @@
 domains=(ireminder.me www.ireminder.me)
 rsa_key_size=4096
 data_path="../certbot"
-email="g.said.alievich@mail.ru" # Adding a valid address is strongly recommended
+email="gadjievsaid.1995@gmail.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
