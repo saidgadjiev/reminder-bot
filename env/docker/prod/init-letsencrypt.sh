91bcb958280be4a89e78260d192565b96fd30fb9
GadzhievSA <s.gadzhiev@id-mt.ru>
2020-02-05 13:51:06 +0300

    Подготовка к продуктиву

--- env/docker/prod/init-letsencrypt.sh
+++ env/docker/prod/init-letsencrypt.sh
@@ -2,7 +2,7 @@
 
 domains=(ireminder.me www.ireminder.me)
 rsa_key_size=4096
-data_path="../certbot"
+data_path="../../../../data/certbot"
 email="gadjievsaid.1995@gmail.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
