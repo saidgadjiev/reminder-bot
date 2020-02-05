b84c9375c107173515b94d452d4ed6308ce7f6c3
GadzhievSA <s.gadzhiev@id-mt.ru>
2020-02-05 23:48:52 +0300

    Успешный запуск на продуктиве

--- env/migration/V1__Reminder.sql
+++ env/migration/V1__Reminder.sql
@@ -15,9 +15,9 @@ CREATE TABLE IF NOT EXISTS reminder
     reminder_text       TEXT         NOT NULL,
     creator_id          INTEGER      NOT NULL REFERENCES tg_user (user_id) ON DELETE CASCADE,
     receiver_id         INTEGER      NOT NULL REFERENCES tg_user (user_id) ON DELETE CASCADE,
-    remind_at           datetime NOT NULL,
+    remind_at           datetime     NOT NULL,
     repeat_remind_at    repeat_time[],
-    initial_remind_at   datetime NOT NULL,
+    initial_remind_at   datetime     NOT NULL,
     status              INT          NOT NULL DEFAULT 0,
     note                TEXT,
     completed_at        TIMESTAMP(0),
@@ -41,5 +41,5 @@ CREATE TABLE IF NOT EXISTS reminder_time
     last_reminder_at TIMESTAMP(0),
     custom           BOOLEAN NOT NULL DEFAULT FALSE,
     reminder_id      INTEGER NOT NULL REFERENCES reminder (id) ON DELETE CASCADE,
-    message_id       INT     NOT NULL
+    its_time         BOOLEAN NOT NULL DEFAULT FALSE
 );
