b292d088b3052b7a20b7817bff022b180b020907
GadzhievSA <s.gadzhiev@id-mt.ru>
2020-02-06 19:29:30 +0300

    Нехватающие миграции

--- env/migration/V5__User_reminder_notification.sql
+++ env/migration/V5__User_reminder_notification.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS user_reminder_notification
 (
     id      SERIAL PRIMARY KEY,
-    day     INT NOT NULL DEFAULT 0,
-    hour    INT NOT NULL DEFAULT 0,
-    minute  INT NOT NULL DEFAULT 0,
+    days     INT NOT NULL DEFAULT 0,
+    hours    INT NOT NULL DEFAULT 0,
+    minutes  INT NOT NULL DEFAULT 0,
     time    TIME,
     type    INT NOT NULL,
     user_id INT NOT NULL REFERENCES tg_user (user_id)
