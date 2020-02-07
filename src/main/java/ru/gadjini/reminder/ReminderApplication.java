75a3e015f029f49b7ff5368f4e3d09988aa0fa08
GadzhievSA <s.gadzhiev@id-mt.ru>
2020-02-07 09:47:12 +0300

    Логирование временное параметров базы

--- src/main/java/ru/gadjini/reminder/ReminderApplication.java
+++ src/main/java/ru/gadjini/reminder/ReminderApplication.java
@@ -2,8 +2,11 @@ package ru.gadjini.reminder;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -23,7 +26,7 @@ import java.util.concurrent.ForkJoinPool;
         WebMoneyProperties.class
 })
 @EnableScheduling
-public class ReminderApplication {
+public class ReminderApplication implements CommandLineRunner {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ReminderApplication.class);
 
@@ -48,5 +51,15 @@ public class ReminderApplication {
         LOGGER.info("CPU Core: " + Runtime.getRuntime().availableProcessors());
         LOGGER.info("CommonPool Parallelism: " + ForkJoinPool.commonPool().getParallelism());
         LOGGER.info("CommonPool Common Parallelism: " + ForkJoinPool.getCommonPoolParallelism());
+    }
+
+    @Autowired
+    private DataSourceProperties dataSourceProperties;
+
+    @Override
+    public void run(String... args) {
+        LOGGER.info("Url: " + dataSourceProperties.getUrl());
+        LOGGER.info("User: " + dataSourceProperties.getUsername());
+        LOGGER.info("Password: " + dataSourceProperties.getPassword());
     }
 }
