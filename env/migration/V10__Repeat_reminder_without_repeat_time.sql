ce2566290c2535ea278701d2c42d1a39d1515bb8
GadzhievSA <s.gadzhiev@id-mt.ru>
2020-04-19 11:58:38 +0300

    Так же снимаем констрейнты с completed_reminder

--- env/migration/V10__Repeat_reminder_without_repeat_time.sql
+++ env/migration/V10__Repeat_reminder_without_repeat_time.sql
@@ -1,2 +1,5 @@
 ALTER TABLE reminder ALTER COLUMN remind_at DROP NOT NULL;
 ALTER TABLE reminder ALTER COLUMN initial_remind_at DROP NOT NULL;
+
+ALTER TABLE completed_reminder ALTER COLUMN remind_at DROP NOT NULL;
+ALTER TABLE completed_reminder ALTER COLUMN initial_remind_at DROP NOT NULL;
