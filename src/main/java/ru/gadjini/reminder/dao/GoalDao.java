d916129624c981a5c2b45db51433c5c04fd6fd1f
GadzhievSA <s.gadzhiev@id-mt.ru>
2021-06-17 22:37:53 +0300

    PMD

--- src/main/java/ru/gadjini/reminder/dao/GoalDao.java
+++ src/main/java/ru/gadjini/reminder/dao/GoalDao.java
@@ -1,6 +1,5 @@
 package ru.gadjini.reminder.dao;
 
-import org.checkerframework.checker.units.qual.A;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
